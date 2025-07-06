949c826fc79fe9362375c64ec772e250a47b40e5
Jhedver <jhedver.tan.22@usjr.edu.ph>
2025-07-06 23:58:55 +0800

    updated folder structure, view and app integration

--- baybayin_backend/baybayin_backend/urls.py
+++ baybayin_backend/baybayin_backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('transliteration.urls'))
+    path('', include('transliteration.urls')),
+    path("spelling/", include("tagalog_spelling_checker.urls"))
 ]
